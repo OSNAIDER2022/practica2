f9d89298c0be34b273cef773d7ba1f3369f31715
OSNAIDER2022 <osnaider.ossoluciones@gmail.com>
2022-05-29 00:15:33 -0500

    se realizan cambios para la futura socializacion con el grupo

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+//Esta constante siempre debe ser declarada al principio del codigo
+//a fin de permitir el correcto funcionamiento de PROMPT
+const prompt = require("prompt-sync")({ sigint: true });
+
+// calculadora en javascript
+
+//... se dejará para la futura socializacion con el grupo
